1b298adaf623fdfd9cc1c6ef48d74e20b767181d
NicolasAltamirano890 <nicolasaltamiranoezequiel2003@gmail.com>
2024-11-18 22:28:59 -0300

    modifiqué el services.py nada más. Nicolás

--- app/layers/services/services.py
+++ app/layers/services/services.py
@@ -2,21 +2,38 @@
 
 from ..persistence import repositories
 from ..utilities import translator
+from ..transport import transport as lista_images
+
 from django.contrib.auth import get_user
 
 def getAllImages(input=None):
     # obtiene un listado de datos "crudos" desde la API, usando a transport.py.
-    json_collection = []
+    lista_images=getAllImages
+    json_collection = [lista_images]
 
     # recorre cada dato crudo de la colección anterior, lo convierte en una Card y lo agrega a images.
     images = []
+
+    for character in json_collection:
+        card = {
+        'id': character['id'],
+        'name': character['name'],
+        'image': character['image'],
+        'status': character['status'],
+    }
+    images.append(card)
 
     return images
 
 # añadir favoritos (usado desde el template 'home.html')
 def saveFavourite(request):
-    fav = '' # transformamos un request del template en una Card.
-    fav.user = '' # le asignamos el usuario correspondiente.
+    fav = {
+    'id': request.POST.get('id'),
+    'name':request.POST.get('name'),
+    'image':request.POST.get('image'),
+    'status':request.POST.get('status')
+    }
+    fav.user = request.user # le asignamos el usuario correspondiente.
 
     return repositories.saveFavourite(fav) # lo guardamos en la base.
 
@@ -27,11 +44,16 @@ def getAllFavourites(request):
     else:
         user = get_user(request)
 
-        favourite_list = [] # buscamos desde el repositories.py TODOS los favoritos del usuario (variable 'user').
+        favourite_list = [repositories.getFavouritesByUser(User)] # buscamos desde el repositories.py TODOS los favoritos del usuario (variable 'user').
         mapped_favourites = []
 
         for favourite in favourite_list:
-            card = '' # transformamos cada favorito en una Card, y lo almacenamos en card.
+            card = {
+            'id':favourite.id,
+            'name':favourite.name,
+            'image':favourite.image,
+            'status':favourite.status,
+            } # transformamos cada favorito en una Card, y lo almacenamos en card.
             mapped_favourites.append(card)
 
         return mapped_favourites
