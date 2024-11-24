c5953ac317af3346d61eefaf86937d21ff67c933
CardozoLeonardo <leonardocardozo699@gmail.com>
2024-11-24 12:32:12 -0300

    views.py posible solución 3

--- app/views.py
+++ app/views.py
@@ -4,32 +4,20 @@ from django.shortcuts import redirect, render
 from .layers.services import services
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-#Importamos requests y json para extraer la información del link y trabajar con ella
-import requests
+from app.layers.transport import transport
 import json
 
 def index_page(request):
     return render(request, 'index.html')
 
-# Home: Lista de imagenes y favoritos
+# esta función obtiene 2 listados que corresponden a las imagenes de la API y los favoritos del usuario y los usa para dibujar el correspondiente template.
+#si el opcional de favoritos no está desarrollado devuelve un listado vacio 
 def home(request):
-    #Lista para guardar las imagenes de todos los personajes
+
     images = [] 
-    
-    #Variable para guardar el link con la información de todos los personajes
-    link=("https://rickandmortyapi.com/api/character")
+    for objeto in transport.getAllImages():
+        images.append(objeto['image'])
 
-    #Obtiene la información de los personajes
-    contenido_link=requests.get(link)
-
-    # Hace manipulable la información de los personajes
-    data=json.loads(contenido_link.content)
-
-    #Recorre la información de todos los personajes
-    for element in data:
-        images.append(element["images"])#Guarda la imagen de todos los personajes en la lista "images"
-
-    # Lista de favoritos (vacía por ahora)
     favourite_list = []
 
     return render(request, 'home.html', {"images": images, "favourite_list": favourite_list})
